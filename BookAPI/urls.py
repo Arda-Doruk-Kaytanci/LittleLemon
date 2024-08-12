c9a2fdd7a1dc87a8e695820180dc42d6e83dfe84
Arda-Doruk-Kaytanci <ardadorukkaytanci@gmail.com>
2024-08-12 10:03:00 +0300

    aadwa

--- BookAPI/urls.py
+++ BookAPI/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('category', views.CategoryView.as_view()), 
     path('user', views.UserView.as_view()),
     path('register', views.register_view, name='register'),
-    path('login', views.login_view, name='login'),
+    path('login', views.user_login, name='login'),
     path('login/manage_staff', views.manage_staff_view, name='manage_staff')
 ]
--- BookAPI/views.py
+++ BookAPI/views.py
@@ -4,6 +4,7 @@ from .models import MenuItem, Category
 from .serializers import MenuSerializer, CategorySerializer, UserSerializer, LoginSerializer, RegisterSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework import generics, permissions
@@ -55,28 +56,26 @@ def register_view(request):
 
     return render(request, 'BookAPI/register.html')
 @csrf_protect
-def login_view(request):
+def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
 
-        serializer = LoginSerializer(data={
-            'username': username,
-            'password': password
-        })
-        if serializer.is_valid():
-            user = serializer.validated_data
+        if user is not None:
+            login(request, user)
+            # Generate new tokens
             refresh = RefreshToken.for_user(user)
             access_token = str(refresh.access_token)
             refresh_token = str(refresh)
 
-            # Return the tokens and redirect or render as needed
-            response = render(request, 'BookAPI/login.html', {'message': 'Login successful'})
-            response.set_cookie('access', access_token, httponly=True)
-            response.set_cookie('refresh', refresh_token, httponly=True)
+            # Set new tokens in cookies
+            response = redirect('/api/menu')  # Redirect to a protected page or home
+            response.set_cookie('access', access_token, httponly=True, secure=True)
+            response.set_cookie('refresh', refresh_token, httponly=True, secure=True)
             return response
         else:
-            return render(request, 'BookAPI/login.html', {'errors': serializer.errors})
+            return render(request, 'BookAPI/login.html', {'error': 'Invalid credentials'})
 
     return render(request, 'BookAPI/login.html')
 @api_view(['GET', 'POST'])
